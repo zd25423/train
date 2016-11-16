68e5b81554011d3e6428953cc2a4ce7c8b85b754
-zd-knight <363726721@qq.com>
2016-11-16 17:01:57 +0800

    fainal version

--- parse_station.py
+++ parse_station.py
@@ -5,5 +5,5 @@ from pprint import pprint
 
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.8971'
 response = requests.get(url,verify=False)
-print(response.text)
-# stations = re.findall(u'')
+stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)' , response.text)
+pprint(dict(stations),indent=4)
